d7bcd464da3b9c6a47c288eb30bd8f1e6ae236a6
ClaudiaYLopezLafita <claudiayll94@gmail.com>
2023-01-24 13:52:11 +0100

    video

---
+++ Unidad 4: Integración de contenido interactivo/Blog en Bootstrap - Evaluable/assets/js/utils.js
@@ -0,0 +1,26 @@
+let video = document.getElementById("myVideo");
+let btn = document.getElementById("myBtn");
+let btn2 = document.getElementById("myBtn");
+let h2 = document.getElementById("h2");
+let content = document.getElementById("textContent");
+
+// btn.addEventListener("click",pauseVideo,true)
+btn2.addEventListener("click",closeVideo, true)
+
+function closeVideo() {
+    video.className = "videoNone";
+    video.muted = true;
+    btn.className = "videoNone";
+    // h2.className = "videoNone";
+    content.className = "videoNone";
+}
+
+function pauseVideo() {
+    if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+    } else {
+    video.pause();
+    btn.innerHTML = "Play";
+    }
+}
