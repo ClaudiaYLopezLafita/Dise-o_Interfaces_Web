371f44d6b0c5e871faed4d009873429ccd37c931
ClaudiaYLopezLafita <claudiayll94@gmail.com>
2023-01-24 19:36:30 +0100

    Responsive y organizacion

--- Unidad 4: Integración de contenido interactivo/Blog en Bootstrap - Evaluable/assets/js/utils.js
+++ Unidad 4: Integración de contenido interactivo/Blog en Bootstrap - Evaluable/assets/js/utils.js
@@ -1,11 +1,11 @@
 let video = document.getElementById("myVideo");
 let btn = document.getElementById("myBtn");
-let btn2 = document.getElementById("myBtn");
+let btn2 = document.getElementById("myBtn2");
 let h2 = document.getElementById("h2");
 let content = document.getElementById("textContent");
 
-// btn.addEventListener("click",pauseVideo,true)
-btn2.addEventListener("click",closeVideo, true)
+btn2.addEventListener("click",pauseVideo,true)
+btn.addEventListener("click",closeVideo, true)
 
 function closeVideo() {
     video.className = "videoNone";
@@ -18,9 +18,9 @@ function closeVideo() {
 function pauseVideo() {
     if (video.paused) {
     video.play();
-    btn.innerHTML = "Pause";
+    btn2.innerHTML = "Pause";
     } else {
     video.pause();
-    btn.innerHTML = "Play";
+    btn2.innerHTML = "Play";
     }
 }
